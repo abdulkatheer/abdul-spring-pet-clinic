bea47c881c3f40baf3b55f1a33cbb97db7a6d8fd
Mohideen Abdul Katheer Mohamed Amsa <MohideenAbdulKatheer.MohamedAmsa@ig.com>
2019-12-19 12:09:29 +0530

    Added address properties to Owner. Closes #26.

--- pet-clinic-data/src/main/java/org/abdul/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/org/abdul/petclinic/model/Owner.java
@@ -3,7 +3,44 @@ package org.abdul.petclinic.model;
 import java.util.Set;
 
 public class Owner extends Person {
+
+    private String address;
+    private String city;
+    private String pincode;
+    private String mobileNo;
     private Set<Pet> pets;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPincode() {
+        return pincode;
+    }
+
+    public void setPincode(String pincode) {
+        this.pincode = pincode;
+    }
+
+    public String getMobileNo() {
+        return mobileNo;
+    }
+
+    public void setMobileNo(String mobileNo) {
+        this.mobileNo = mobileNo;
+    }
 
     public Set<Pet> getPets() {
         return pets;
