9410cd41d1c213d342270949031cff958db82eeb
Mohideen Abdul Katheer Mohamed Amsa <MohideenAbdulKatheer.MohamedAmsa@ig.com>
2019-12-23 15:27:03 +0530

    Added SD Jpa Implementation for VisitService. Closes #44.

---
+++ pet-clinic-data/src/main/java/org/abdul/petclinic/service/springdatajpa/VisitSDJpaService.java
@@ -0,0 +1,48 @@
+package org.abdul.petclinic.service.springdatajpa;
+
+import org.abdul.petclinic.model.Visit;
+import org.abdul.petclinic.repository.VisitRepository;
+import org.abdul.petclinic.service.VisitService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+@Service
+@Profile("springdatajpa")
+public class VisitSDJpaService implements VisitService {
+    private final VisitRepository visitRepository;
+
+    public VisitSDJpaService(VisitRepository visitRepository) {
+        this.visitRepository = visitRepository;
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        return StreamSupport
+                .stream(visitRepository.findAll().spliterator(), false)
+                .collect(Collectors.toSet());
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return visitRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        return visitRepository.save(visit);
+    }
+
+    @Override
+    public void delete(Visit visit) {
+        visitRepository.delete(visit);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        visitRepository.deleteById(id);
+    }
+}
