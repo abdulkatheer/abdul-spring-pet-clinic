280a9a2af2e29103512b1b887dd33759f1c3a5e6
Mohideen Abdul Katheer Mohamed Amsa <MohideenAbdulKatheer.MohamedAmsa@ig.com>
2019-12-23 14:43:59 +0530

    Added SD Jpa Implementation for SpecialityService. Closes #40.

---
+++ pet-clinic-data/src/main/java/org/abdul/petclinic/service/springdatajpa/SpecialitySDJpaService.java
@@ -0,0 +1,48 @@
+package org.abdul.petclinic.service.springdatajpa;
+
+import org.abdul.petclinic.model.Speciality;
+import org.abdul.petclinic.repository.SpecialityRepository;
+import org.abdul.petclinic.service.SpecialityService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+@Service
+@Profile("springdatajpa")
+public class SpecialitySDJpaService implements SpecialityService {
+    private final SpecialityRepository specialityRepository;
+
+    public SpecialitySDJpaService(SpecialityRepository specialityRepository) {
+        this.specialityRepository = specialityRepository;
+    }
+
+    @Override
+    public Set<Speciality> findAll() {
+        return StreamSupport
+                .stream(specialityRepository.findAll().spliterator(), false)
+                .collect(Collectors.toSet());
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return specialityRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Speciality save(Speciality speciality) {
+        return specialityRepository.save(speciality);
+    }
+
+    @Override
+    public void delete(Speciality speciality) {
+        specialityRepository.delete(speciality);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        specialityRepository.deleteById(id);
+    }
+}
