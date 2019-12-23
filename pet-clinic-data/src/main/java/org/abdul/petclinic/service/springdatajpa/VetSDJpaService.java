8ae279ede5993d8ec2a83148889fd55499f092ba
Mohideen Abdul Katheer Mohamed Amsa <MohideenAbdulKatheer.MohamedAmsa@ig.com>
2019-12-23 14:33:04 +0530

    Added SD Jpa Implementation for VetService. Closes #39.

---
+++ pet-clinic-data/src/main/java/org/abdul/petclinic/service/springdatajpa/VetSDJpaService.java
@@ -0,0 +1,48 @@
+package org.abdul.petclinic.service.springdatajpa;
+
+import org.abdul.petclinic.model.Vet;
+import org.abdul.petclinic.repository.VetRepository;
+import org.abdul.petclinic.service.VetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+@Service
+@Profile("springdatajpa")
+public class VetSDJpaService implements VetService {
+    private final VetRepository vetRepository;
+
+    public VetSDJpaService(VetRepository vetRepository) {
+        this.vetRepository = vetRepository;
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+        return StreamSupport
+                .stream(vetRepository.findAll().spliterator(), false)
+                .collect(Collectors.toSet());
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return vetRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Vet save(Vet owner) {
+        return vetRepository.save(owner);
+    }
+
+    @Override
+    public void delete(Vet owner) {
+        vetRepository.delete(owner);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        vetRepository.deleteById(id);
+    }
+}
