6dbcf98d74651e7e7ae07414adb20235e51eb4dc
Mohideen Abdul Katheer Mohamed Amsa <MohideenAbdulKatheer.MohamedAmsa@ig.com>
2019-12-23 12:58:33 +0530

    Converted Owner to JPA Entity. Closes #34.

--- pet-clinic-data/src/main/java/org/abdul/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/org/abdul/petclinic/model/Owner.java
@@ -1,14 +1,26 @@
 package org.abdul.petclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "owners")
 public class Owner extends Person {
 
+    @Column(name = "address")
     private String address;
+
+    @Column(name = "city")
     private String city;
+
+    @Column(name = "pincode")
     private String pincode;
+
+    @Column(name = "mobile_no")
     private String mobileNo;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
     private Set<Pet> pets;
 
     public Owner() {
